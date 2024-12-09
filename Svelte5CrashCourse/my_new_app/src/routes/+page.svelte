84f38f21b2100e984cac3db9eaea931f131355e8
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-09 21:32:31 +0530

    Basic edit to html page

--- Svelte5CrashCourse/my_new_app/src/routes/+page.svelte
+++ Svelte5CrashCourse/my_new_app/src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => console.log('clicked')}>Click me</button>
+
+<style>
+	h1 {
+		color: blue;
+	}
+</style>
