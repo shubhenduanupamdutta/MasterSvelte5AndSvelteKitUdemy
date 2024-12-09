0816ac79a51152389f63b8d2442517091d9c26bf
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-09 21:34:40 +0530

    Using code from inside script tag.

--- Svelte5CrashCourse/my_new_app/src/routes/+page.svelte
+++ Svelte5CrashCourse/my_new_app/src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+    function onClick() {
+        console.log("Clicked from inside the script tag");
+    }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <button onclick={() => console.log('clicked')}>Click me</button>
+<button onclick={onClick}>Click me from inside the script tag</button>
 
 <style>
 	h1 {
