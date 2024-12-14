79b9fa35298f61676b1f2c63bf1042dcfe691849
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-14 19:32:01 +0530

    Proj1 EbookLanding - Hero Section CSS - My Screen

--- Project_1/ebook-landing/src/lib/components/HeroSection.svelte
+++ Project_1/ebook-landing/src/lib/components/HeroSection.svelte
@@ -22,3 +22,40 @@
 		</p>
 	</div>
 </section>
+
+<style>
+	.hero {
+		display: flex;
+		height: 100vh;
+		overflow-y: hidden;
+	}
+
+	.hero-showcase {
+		position: relative;
+		width: 50%;
+		padding: 100px 16px 0 16px;
+		background: linear-gradient(135deg, #e2cbff 25%, #deeaff 50%, #ece5ff 75%, #927bce);
+	}
+
+	.hero-text {
+		padding: 100px 5%;
+		width: 50%;
+		background-color: black;
+	}
+
+    .book-cover {
+        position: absolute;
+        right: 2%;
+        bottom: 0;
+        width: 50%;
+        transform: translate(-25%, 30%);
+    }
+
+    .phone-cover {
+        position: absolute;
+        width: 25%;
+        bottom: 0;
+        right: 52%;
+        transform: translate(-10%, 15%);
+    }
+</style>
