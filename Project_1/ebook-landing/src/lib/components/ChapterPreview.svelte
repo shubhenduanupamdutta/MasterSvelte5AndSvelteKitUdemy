43ba67c12082fc958260bf118f5747972ecec7ea
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-14 20:16:11 +0530

    Proj1 EbookLanding - Chapter Preview - Styling

--- Project_1/ebook-landing/src/lib/components/ChapterPreview.svelte
+++ Project_1/ebook-landing/src/lib/components/ChapterPreview.svelte
@@ -3,7 +3,11 @@
 	<div class="chapter-container">
 		<ul>
 			<li>
-				<button class="chapter-title" aria-controls="chapter-info-1" aria-expanded="true">
+				<button
+					class="chapter-title selected-chapter-title"
+					aria-controls="chapter-info-1"
+					aria-expanded="true"
+				>
 					<h3>Chapter 1: Navigating the Spanish Bureaucracy</h3>
 				</button>
 			</li>
@@ -12,9 +16,57 @@
 			<h3 class="chapter-strapline italic mb-s">
 				Understanding Visa, Residency and Legal Essentials
 			</h3>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, tempore! Quidem, pariatur voluptatibus ex laborum ipsa blanditiis nulla nihil maiores iste deleniti suscipit, veniam a, vel tenetur. Quam, asperiores saepe! Lorem ipsum dolor sit amet consectetur, adipisicing elit. In soluta non architecto laboriosam exercitationem, illum, molestias quod corrupti enim nemo ipsum praesentium quasi ipsa obcaecati pariatur, amet possimus mollitia nam.
-            </p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, tempore! Quidem, pariatur
+				voluptatibus ex laborum ipsa blanditiis nulla nihil maiores iste deleniti suscipit, veniam
+				a, vel tenetur. Quam, asperiores saepe! Lorem ipsum dolor sit amet consectetur, adipisicing
+				elit. In soluta non architecto laboriosam exercitationem, illum, molestias quod corrupti
+				enim nemo ipsum praesentium quasi ipsa obcaecati pariatur, amet possimus mollitia nam. Lorem
+				ipsum dolor sit amet consectetur adipisicing elit. Facilis, facere asperiores. Quasi,
+				officiis, quas veniam quisquam itaque, cum id provident ipsum eum beatae ullam error. Autem
+				expedita ipsam odio vero? Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+				ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ratione sapiente assumenda
+				impedit necessitatibus architecto neque consequatur quaerat ipsa minus!
+			</p>
 		</div>
 	</div>
 </section>
+
+<style>
+	.chapter-preview {
+		padding: 80px 0;
+		margin-left: 9vw;
+		margin-right: 15vw;
+		max-width: 1150px;
+	}
+
+	.chapter-container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.chapter-container ul {
+		width: 40%;
+	}
+
+	.chapter-info {
+		width: 55%;
+	}
+
+	.chapter-title {
+		border-bottom: 1px solid grey;
+		width: 100%;
+		display: block;
+		padding: 10px;
+		text-align: left;
+	}
+
+	.selected-chapter-title {
+		background-color: black;
+		border: none;
+		color: white;
+		box-shadow:
+			0 4px 6px rgba(0, 0, 0, 0.1),
+			0 1px 3px rgba(0, 0, 0, 0.08);
+	}
+</style>
