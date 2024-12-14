1aa64cbe372c535beff1169a86bed8d162e62362
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-14 14:57:58 +0530

    Proj1 EbookLanding - Getting Project Assets

--- Project_1/ebook-landing/src/lib/components/HeroSection.svelte
+++ Project_1/ebook-landing/src/lib/components/HeroSection.svelte
@@ -1,5 +1,7 @@
 <section class="hero">
 	<div class="hero-showcase text-center bold">
-		<h1 class="purple">Move To</h1>
+		<h1 class="purple">MOVE TO</h1>
+		<h1 class="purple mb-xs">SPAIN</h1>
+		<h4 class="italic semi-bold">There's no need to make same mistakes I made.</h4>
 	</div>
 </section>
