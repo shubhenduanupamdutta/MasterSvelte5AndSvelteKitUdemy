6932bbb144b4fb73e1ddb7dba16b202c3e3369dd
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-12-14 21:39:27 +0530

    Proj1 EbookLanding - Author Section - CSS

--- Project_1/ebook-landing/src/lib/components/AuthorSection.svelte
+++ Project_1/ebook-landing/src/lib/components/AuthorSection.svelte
@@ -6,7 +6,7 @@
 	<h2 class="mb-l">About the Author</h2>
 	<div class="author-container">
 		<div class="author-text">
-			<p>
+			<p class="mb-xs">
 				Shubhendu Dutta is an accomplished international relocation consultant with over a decade of
 				firsthand experience navigating expatriate life across Europe. Having personally relocated
 				to Spain in 2015, he brings a unique blend of professional expertise and personal insights
@@ -23,3 +23,29 @@
 		<img src={authorPic} alt="Author Shubhendu A. Dutta" />
 	</div>
 </section>
+
+<style>
+	.author-section {
+		padding: 80px 0;
+		margin-left: 9vw;
+		margin-right: 15vw;
+		max-width: 1150px;
+        height: 100vh;
+        overflow: hidden;
+	}
+
+	.author-container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.author-container img {
+		width: 40%;
+		border: 6px solid black;
+		box-shadow: 0 4px 24px rgba(0, 0, 0, 0.5);
+	}
+
+	.author-text {
+		width: 55%;
+	}
+</style>
